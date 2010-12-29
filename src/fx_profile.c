70c056f1ccb447a389a1a89fd673c031eb611ed8
Aron Xu <happyaron.xu@gmail.com>
2010-12-29 14:26:42 +0800

    Add OpenSSL exception terms to UI sources.

--- src/fx_profile.c
+++ src/fx_profile.c
@@ -1,21 +1,32 @@
 /***************************************************************************
- *   copyright (c) 2010 by lwp                                             *
+ *   Copyright (C) 2010 by lwp                                             *
  *   levin108@gmail.com                                                    *
  *                                                                         *
- *   this program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the gnu general public license as published by  *
- *   the free software foundation; either version 2 of the license, or     *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
- *   this program is distributed in the hope that it will be useful,       *
- *   but without any warranty; without even the implied warranty of        *
- *   merchantability or fitness for a particular purpose.  see the         *
- *   gnu general public license for more details.                          *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
  *                                                                         *
- *   you should have received a copy of the gnu general public license     *
+ *   You should have received a copy of the GNU General Public License     *
  *   along with this program; if not, write to the                         *
- *   free software foundation, inc.,                                       *
- *   59 temple place - suite 330, boston, ma  02111-1307, usa.             *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Suite 500, Boston, MA 02110-1335, USA.            *
+ *                                                                         *
+ *   OpenSSL linking exception                                             *
+ *   --------------------------                                            *
+ *   If you modify this Program, or any covered work, by linking or        *
+ *   combining it with the OpenSSL project's "OpenSSL" library (or a       *
+ *   modified version of that library), containing parts covered by        *
+ *   the terms of OpenSSL/SSLeay license, the licensors of this            *
+ *   Program grant you additional permission to convey the resulting       *
+ *   work. Corresponding Source for a non-source form of such a            *
+ *   combination shall include the source code for the parts of the        *
+ *   OpenSSL library used as well as that of the covered work.             *
  ***************************************************************************/
 
 #include "fx_include.h"
